ff69aec0af78705b6bf1b81f104a339c6e6db247
Hip <jean-philippe.baconnais@pole-emploi.fr>
2018-03-26 09:55:29 +0200

    Add variable declaration

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Printf("Hello world")
+	var message string    	// manual type déclaration
+
+	// var args []string
+	args := os.Args 		// type inference
+
+	if len(args) > 1 {
+		message = args[1]
+	} else {
+		message = "Texte par défaut"
+	}
+
+	fmt.Printf(message)
+
 }
