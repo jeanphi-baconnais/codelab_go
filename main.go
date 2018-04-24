1609281a8d3810e420017c9119797817be4a45d9
Hip <jean-philippe.baconnais@pole-emploi.fr>
2018-04-24 15:25:17 +0200

    Ajout exercice 4 suite avec goroutine

--- main.go
+++ main.go
@@ -4,19 +4,41 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"io/ioutil"
+	"time"
 )
+
+var infos = make (chan string)
 
 func main() {
 	// Goroutine / channel
-	max := 35
+	max := 5
+
+	log.Printf("Before call ");
+	for i := 0; i < max; i++ {
+		go callApiRest(i)
+	}
+	log.Printf("After call ");
+
+	time.Sleep( 5 * time.Second)
 
 	for i := 0; i < max; i++ {
-		callApiRest(i)
+		log.Printf("Channel" + <- infos);
 	}
 }
+
+
 func callApiRest(value int) {
 	url := "http://localhost:9999/generate/" +strconv.Itoa(value)
+
 	log.Printf("Call : %s", url)
-	http.Get(url)
+
+	response, _ := http.Get(url)
+
 	log.Printf("Call finished : %s", url)
+
+	body, _ := ioutil.ReadAll(response.Body)
+
+	infos <- string(body)
+
 }
