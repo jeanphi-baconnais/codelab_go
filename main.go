e9bdaaf2d6d2e42370e366242f97febe273a5ace
Hip <jean-philippe.baconnais@pole-emploi.fr>
2018-04-24 16:27:37 +0200

    hello world

--- main.go
+++ main.go
@@ -1,44 +1,15 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"strconv"
-	"io/ioutil"
-	"time"
+	"fmt"
 )
 
 var infos = make (chan string)
 
 func main() {
-	// Goroutine / channel
-	max := 5
-
-	log.Printf("Before call ");
-	for i := 0; i < max; i++ {
-		go callApiRest(i)
-	}
-	log.Printf("After call ");
-
-	time.Sleep( 5 * time.Second)
-
-	for i := 0; i < max; i++ {
-		log.Printf("Channel" + <- infos);
-	}
+	// Defer
+	defer fmt.Println("world")
+	fmt.Println("hello")
 }
 
 
-func callApiRest(value int) {
-	url := "http://localhost:9999/generate/" +strconv.Itoa(value)
-
-	log.Printf("Call : %s", url)
-
-	response, _ := http.Get(url)
-
-	log.Printf("Call finished : %s", url)
-
-	body, _ := ioutil.ReadAll(response.Body)
-
-	infos <- string(body)
-
-}
