f2cabbeea504eb51151e0b0c764882a0020f6281
Hip <jean-philippe.baconnais@pole-emploi.fr>
2018-04-25 08:16:42 +0200

    create functions

--- main.go
+++ main.go
@@ -1,19 +1,38 @@
 package main
 
-import (
-	"fmt"
-)
-
-var infos = make (chan string)
+import "errors"
 
 func main() {
-	fmt.Println("counting")
 
-	for i := 0; i < 10; i++ {
-		defer fmt.Println(i)
-	}
-
-	fmt.Println("done")
 }
 
+type person struct {
+	name string
+	age  int
+}
 
+func Sum (x int, y int) int {
+	return x + y
+}
+
+/*
+	Fonctions à tester.
+ */
+
+// Valeurs de retour multiples
+func ReturnValeurAndValuesMutlipliees(x int, y int) (int, int) {
+	return x, x*y
+}
+
+// Création d'une personne
+func CreatePersonne(name string, age int) person {
+	return person {name, age}
+}
+
+// Tests des erreurs
+func Function42throwError(arg int) (int, error) {
+	if arg == 42 {
+		return -1, errors.New("Error, not 42 !")
+	}
+	return arg + 3, nil
+}
